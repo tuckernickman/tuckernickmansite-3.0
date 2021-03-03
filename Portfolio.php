dd307e6f4cfeaa143dd9a1a452c8f087db692aa8
tuckernickman <tucker.nickman@gmail.com>
2021-03-02 18:12:09 -0700

    php

---
+++ Portfolio.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tucker Nickman - Portfolio Website 2021</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/portfolio.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid" id="grad">
+          <a class="navbar-brand" href="Index.html">Tucker Nickman</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <ul class="navbar-nav mr-auto">
+               <li class="nav-item active">
+                    <a class="nav-link active" aria-current="page" href="Index.html">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="about.html">About</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="archive.html">Archive</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="Portfolio.html">Portfolio</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="beginning.html">Biomorph</a>
+                </li>
+                
+            </ul>
+            </div>
+          </div>
+        </div>
+    </nav>
+
+    <!--home page-->
+
+    <div id="main-wrapper" class="layout-main-wrapper">
+        <div id="main" class="container-fluid">
+            <div class="row row-offcanvas row-offcanvas-left clearfix">
+                <main class="main-content col" id="content" role="main">
+                    <section class="section">
+                        <a id="main-content" tabindex="-1"></a>
+                        <div  class="block block-system block-system-main-block">
+                            <div class="block-inner">
+                                <div class="content">
+                                    <div class="sections">
+                                        <section>
+                                            <h2>Portfolio</h2>
+                                            <div class="section-content perspective">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <a href="2021.html" class="field-group-link project-link d-block p-visible">
+                                                            <div class="project-img bg">
+                                                                <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles and cyptobiotic soil">
+                                                            </div>
+                                                            <div class="project-year">
+                                                                <small>
+                                                                    Needles Biomorph
+                                                                </small>
+                                                            </div>
+                                                            <h3 class="font1 font-change">2021</h3>
+                                                        </a>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <a href="2020.html" class="field-group-link project-link d-block p-visible">
+                                                            <div class="project-img bg">
+                                                                <img class="img-fluid" src="assets/img/2020/vogelkorper.jpg" alt="bird carcass">
+                                                            </div>
+                                                            <div class="project-year">
+                                                                <small>
+                                                                    Carcass
+                                                                </small>
+                                                            </div>
+                                                            <h3 class="font1 font-change">2020</h3>
+                                                        </a>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <a href="2019.html" class="field-group-link project-link d-block p-visible">
+                                                            <div class="project-img bg">
+                                                                <img class="img-fluid" src="assets/img/2019/Wuste.jpg" alt="desert image 2019">
+                                                            </div>
+                                                            <div class="project-year">
+                                                                <small>
+                                                                    Desert Image
+                                                                </small>
+                                                            </div>
+                                                            <h3 class="font1 font-change">2019</h3>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </section>
+                                        <section>
+                                            <h2>
+                                                Digital Art
+                                            </h2>
+                                            <div class="field field-name-field-paragraph">
+                                                <div class="setion-content">
+                                                    <div class="text-max-width mx-auto onecol">
+
+                                                    </div>
+                                                </div>
+                                            </div> 
+                                        </section>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </main>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript
+    <script src="js\newscript1.js"></script>
+    -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!-- P5.js script-->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/addons/p5.sound.min.js"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.14/p5.js"></script>
+    <script src="js/alert1.js" ></script>
+</body>
+</html>
