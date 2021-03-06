f6e05728bd3eece002090f870c9279fa02989d99
tuckernickman <tucker.nickman@gmail.com>
2021-03-06 15:55:25 -0700

    .3.2

--- section1.php
+++ section1.php
@@ -2,7 +2,7 @@
     <h2>Portfolio</h2>
         <div class="section-content perspective">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-4 container">
                     <a href="/2021.html" class="field-group-link project-link d-block p-visible">
                         <div class="project-img bg">
                             <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles and cyptobiotic soil">
@@ -15,10 +15,10 @@
                             <h3 class="font1 font-change">2021</h3>
                     </a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 container">
                     <a href="/2020.html" class="field-group-link project-link d-block p-visible">
                         <div class="project-img bg">
-                            <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles and cyptobiotic soil">
+                            <img class="img-fluid" src="assets/img/2020/canyonlands.jpg" alt="needles and cyptobiotic soil">
                         </div>
                         <div class="project-year">
                             <small>
@@ -28,14 +28,14 @@
                         <h3 class="font1 font-change">2021</h3>
                     </a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 container">
                     <a href="/2019.html" class="field-group-link project-link d-block p-visible">
                         <div class="project-img bg">
-                            <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles and cyptobiotic soil">
+                            <img class="img-fluid" src="assets/img/2019/bones00072.jpg" alt="bones">
                         </div>
                         <div class="project-year">
                         <small>
-                            Needles Biomorph, 2021
+                            Bones, 2019
                         </small>
                         </div>
                     <h3 class="font1 font-change">2021</h3>
