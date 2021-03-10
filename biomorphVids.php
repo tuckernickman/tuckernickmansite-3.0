a93a87ffe5b5834be32e78fc41be33f4357a1c62
tuckernickman <tucker.nickman@gmail.com>
2021-03-09 19:17:21 -0700

    09.03.2021.01

---
+++ biomorphVids.php
@@ -0,0 +1,18 @@
+<?php 
+    $links = [
+        "2021.php" => ["2021content.php"],
+        "2020.php" => ["2020content.php"],
+        "2019.php" => ["2019content.php"]
+    ];
+
+    $biomorph20 = [
+        "assets/img/2021/gsl_1.1.2.jpg",
+        "assets/img/2021/MOAB_1.1.1.jpg",
+        "assets/img/2021/MOAB_2.2.1.jpg",
+        "assets/img/2021/needles_1.2.jpg",
+        "assets/img/2021/needles_5.jpg",
+        "assets/img/2021/rozel_point5.jpg",
+        "assets/img/2021/silver_island_4.jpg",
+        "assets/img/2021/YS_2.1.1.jpg"
+    ];
+?>
