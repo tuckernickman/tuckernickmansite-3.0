d6c169519d0ff9e828bf822bbeb4aa419dc82c04
tuckernickman <tucker.nickman@gmail.com>
2021-03-15 21:12:11 -0600

    15.03.21.04

--- beginning.php
+++ beginning.php
@@ -17,25 +17,9 @@
 <body>
     <?php include "menu.php"; ?>
 
-
     <!--home page-->
-    <?php include "section1.php"; ?> 
-
-    <div class="featured-bottom">
-        <aside class="container-fluid clearfix" role="complimentary">
-            <section class="row region region-featured-bottom-first">
-                <div id="block-flippyblock" class="block block-flippy block-flippy-block">
-                    <div class="block-inner">
-                        <ul class="content">
-                            
-                        </ul>
-                    </div>
-
-                </div>
-            </section>
-        </aside>
-    </div>
-
+    <?php include "biomorphVids.php"; ?> 
+    
     <?php include "footer.php"; ?>
 </body>
 </html>
