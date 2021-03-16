24b2d5cf762f8eb199158a953235bf718a6bd448
tuckernickman <tucker.nickman@gmail.com>
2021-03-15 18:14:51 -0600

    15.02.21.03

--- biomorphVids.php
+++ biomorphVids.php
@@ -1,3 +1,65 @@
+<section>
+    <h2 id="content">2021</h2>
+    <div class="col section-content perspective">
+    <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/gsl_1.1.2.jpg" alt="Great Salt Lake Biomorph">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/MOAB_1.1.1.jpg" alt="moab biomorph 1">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/MOAB_2.2.1.jpg" alt="moab biomorph 2">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/needles_1.2.jpg" alt="needles biomorph 1">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles biomorph 2">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/rozel_point5.jpg" alt="rozel point biomorph">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/silver_island_4.jpg" alt="silver island biomorph">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2021/YS_2.1.1.jpg" alt="Yellow Stone Biomorph">
+                    </div>
+                </a>
+            </div>
+    </div>
+</section>
+
 <?php 
     // $links = [
     //     "2021.php" => ["2021content.php"],
@@ -5,47 +67,47 @@
     //     "2019.php" => ["2019content.php"]
     // ];
 
-    $biomorph20 = [
-        "assets/img/2021/gsl_1.1.2.jpg",
-        "assets/img/2021/MOAB_1.1.1.jpg",
-        "assets/img/2021/MOAB_2.2.1.jpg",
-        "assets/img/2021/needles_1.2.jpg",
-        "assets/img/2021/needles_5.jpg",
-        "assets/img/2021/rozel_point5.jpg",
-        "assets/img/2021/silver_island_4.jpg",
-        "assets/img/2021/YS_2.1.1.jpg"
-    ];
+    // $biomorph20 = [
+    //     "assets/img/2021/gsl_1.1.2.jpg",
+    //     "assets/img/2021/MOAB_1.1.1.jpg",
+    //     "assets/img/2021/MOAB_2.2.1.jpg",
+    //     "assets/img/2021/needles_1.2.jpg",
+    //     "assets/img/2021/needles_5.jpg",
+    //     "assets/img/2021/rozel_point5.jpg",
+    //     "assets/img/2021/silver_island_4.jpg",
+    //     "assets/img/2021/YS_2.1.1.jpg"
+    // ];
 
-    function loopThrough($strArray) {
-        $imgElement = getElementById('imgMorph');
+    // function loopThrough($strArray) {
+    //     $imgElement = getElementById('imgMorph');
     
-        foreach ($strArray as $i=>$strArray) {
-            $imgElement.setAttribute('src', $strArray[$i]);
-            sleep(10);
-        }
-    }
+    //     foreach ($strArray as $i=>$strArray) {
+    //         $imgElement.setAttribute('src', $strArray[$i]);
+    //         sleep(10);
+    //     }
+    // }
     
-    function prntImgArr($strArray)
-    {
-    foreach ($strArray as $i=>$strArray){
-    $content[$i] = '<div class="col-sm-4">
-    <a href="/2021.php" class="field-group-link project-link d-block p-visible">
-        <div class="project-img bg">
-            <img class="img-fluid" src='.$strArray[$i].'alt="needles and sandstone">
-        </div>
-        <div class="project-year">
-            <small>
-                    Needles Biomorph, 2021
-            </small>
-        </div>
-            <h3 class="font1 font-change">2021</h3>
-    </a>
-    </div>';
-    }
-    foreach($strArray as $i=>$strArray){
-    $rowWrapper = '<div class="row">'.$content[$i].$content[$i+1].$content[$i+2].'</div>';
-    }
+    // function prntImgArr($strArray)
+    // {
+    // foreach ($strArray as $i=>$strArray){
+    // $content[$i] = '<div class="col-sm-4">
+    // <a href="/2021.php" class="field-group-link project-link d-block p-visible">
+    //     <div class="project-img bg">
+    //         <img class="img-fluid" src='.$strArray[$i].'alt="needles and sandstone">
+    //     </div>
+    //     <div class="project-year">
+    //         <small>
+    //                 Needles Biomorph, 2021
+    //         </small>
+    //     </div>
+    //         <h3 class="font1 font-change">2021</h3>
+    // </a>
+    // </div>';
+    // }
+    // foreach($strArray as $i=>$strArray){
+    // $rowWrapper = '<div class="row">'.$content[$i].$content[$i+1].$content[$i+2].'</div>';
+    // }
     
-    return $rowWrapper;
-    }
+    // return $rowWrapper;
+    // }
 ?>
--- imgGrid21.php
+++ imgGrid21.php
@@ -1,78 +1,56 @@
 <section>
-    <h2>2021</h2>
-    <div class="section-content perspective">
-        <div class="row col">
-            <div id="content" class="col-sm-6">
-                <a href="" class="field-group-link project-link d-block p-visible">
+    <h2 id="content">2021</h2>
+    <div class="col section-content perspective">
+    <div id="content" class="col">
+                <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/30076215219495318432762383559479649768809329962657002582229085626228845576193" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/gsl_1.1.2.jpg" alt="Great Salt Lake Biomorph">
                     </div>
-                    <div class="project-year">
-                        <small>
-
-                        </small>
-                    </div>
                 </a>
             </div>
-            <div id="content" class="col-sm-6">
+            <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/MOAB_1.1.1.jpg" alt="moab biomorph 1">
                     </div>
-                    <div class="project-year">
-                        <small>
-
-                        </small>
-                    </div>
                 </a>
             </div>
-            <div id="content" class="col-sm-6">
+            <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/MOAB_2.2.1.jpg" alt="moab biomorph 2">
                     </div>
-                    <div class="project-year">
-                        <small>
-
-                        </small>
-                    </div>
                 </a>
             </div>
-    </div>
-    <div class="section-content perspective">
-        <div class="row col">
-            <div id="content" class="col-sm-6">
-                <a href="" class="field-group-link project-link d-block p-visible">
+            <div id="content" class="col">
+                <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/30076215219495318432762383559479649768809329962657002582229085629527380459521" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/needles_1.2.jpg" alt="needles biomorph 1">
                     </div>
                 </a>
             </div>
-            <div id="content" class="col-sm-6">
+            <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/needles_5.jpg" alt="needles biomorph 2">
                     </div>
                 </a>
             </div>
-            <div id="content" class="col-sm-6">
-                <a href="" class="field-group-link project-link d-block p-visible">
+            <div id="content" class="col">
+                <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/30076215219495318432762383559479649768809329962657002582229085630626892087297" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/rozel_point5.jpg" alt="rozel point biomorph">
                     </div>
                 </a>
             </div>
-    </div>
-    <div class="section-content perspective">
-        <div class="row col">
-            <div id="content" class="col-sm-6">
-                <a href="" class="field-group-link project-link d-block p-visible">
+            <div id="content" class="col">
+                <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/30076215219495318432762383559479649768809329962657002582229085628427868831745" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/silver_island_4.jpg" alt="silver island biomorph">
                     </div>
                 </a>
             </div>
-            <div id="content" class="col-sm-6">
+            <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="project-img bg">
                         <img class="img-fluid" src="assets/img/2021/YS_2.1.1.jpg" alt="Yellow Stone Biomorph">
