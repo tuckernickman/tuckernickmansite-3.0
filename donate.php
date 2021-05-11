f484ffe72c44c767562e73e0633391e097e9850b
tuckernickman <tucker.nickman@gmail.com>
2021-05-11 13:45:48 -0600

    edits

---
+++ donate.php
@@ -0,0 +1,11 @@
+<div style="text-align: center;">
+	<h3>Donate</h3>
+	<span id="donate-bitcoin" class="donate-btn"></span>
+    <span id="donate-ether" class="donate-btn"></span>
+	<span id="donate-cardano" class="donate-btn"></span>
+	<span id="donate-algorand" class="donate-btn"></span>
+    <span id="donate-dogecoin" class="donate-btn"></span>
+    <span id="donate-polkadot" class="donate-btn"></span>
+    <span id="donate-tron" class="donate-btn"></span>
+    <span id="donate-tezos" class="donate-btn"></span>
+</div>
--- header.php
+++ header.php
@@ -3,10 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/main.css">
-    <link rel="stylesheet" href="styles/portfolio.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&family=Roboto+Slab&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap" rel="stylesheet">
+    
     <!-- alt fonts -->
     <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Gugi&family=Modak&family=Pinyon+Script&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+    <!-- cointipper -->
+    <link rel="stylesheet" type="text/css" href="/styles/jquery.cointipper.css">
+    <link rel="stylesheet" type="text/css" href="/styles/jquery.cointipper.min.css">
+
+    <!-- mycss -->
+    <link rel="stylesheet" type="text/css" href="/styles/main.css">
+    <link rel="stylesheet" type="text/css" href="/styles/portfolio.css">
