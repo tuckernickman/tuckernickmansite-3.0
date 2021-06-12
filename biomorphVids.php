f4a3db24dd9ded3e9be7951c9d3fdfb7c5a39bc1
tuckernickman <tucker.nickman@gmail.com>
2021-06-12 14:33:45 -0600

    edits

--- biomorphVids.php
+++ biomorphVids.php
@@ -4,6 +4,13 @@
             <div id="content" class="col">
                 <a href="" class="field-group-link project-link d-block p-visible">
                     <div class="videoWrapper">
+                        <iframe id="content" class="col" src="https://www.youtube.com/embed/HUkvPlpvmv0"w frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" preload="metadata" allowfullscreen></iframe>
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="videoWrapper">
                         <iframe id="content" class="col" src="https://www.youtube.com/embed/dJf7TSWBPI4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" preload="metadata" allowfullscreen></iframe>
                     </div>
                 </a>
