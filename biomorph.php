bd5150d4bcaec5ea93b389330ee7ee93b0a27b3b
tuckernickman <tucker.nickman@gmail.com>
2021-06-26 08:19:38 -0600

    adjustedd layout

--- biomorph.php
+++ biomorph.php
@@ -8,41 +8,73 @@
 <body>
     <?php include "menu.php"; ?>
 
-    <section>
-        <div class="col-lg col-md-8 col-sm-12 mb-5">
-            <h2 class="font1 font-change txt-color"><a href="beginning.php">Beginning</h2>
-            <div id="content" class="col-lg col-md-8 col-sm-12">
-                <a href="beginning.php"><img id="content" class="col-lg col-md-8 col-sm-12" src="/assets/img/biomorph/beginning1.jpg" alt="Beginning Preview Image"></a>
+    <section class="mb-5">
+
+        <div class="col section-content perspective ">
+            <h2 id="content" class="font1 font-change txt-color"><a href="beginning.php">Beginning</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <a href="beginning.php" class="field-group-link project-link d-block p-visible font1 font-change">
+                        <div class="project-img bg">
+                            <img class="img-fluid" src="/assets/img/biomorph/beginning1.jpg" alt="Beginning Preview Image">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col section-content perspective ">
+            <h2 id="content" class="font1 font-change txt-color"><a href="copper.php">Copper</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <a href="copper.php" class="field-group-link project-link d-block p-visible font1 font-change">
+                        <div class="project-img bg">
+                            <img class="img-fluid" src="/assets/img/biomorph/copper1.jpg" alt="Copper Preview Image">
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
-        <div class="col-lg col-md-8 col-sm-12 mb-5">
-            <h2 class="font1 font-change txt-color"><a href="copper.php">Copper</h2>
-            <div id="content" class="col-lg col-md-8 col-sm-12">
-                <a href="copper.php"><img id="content" class="col-lg col-md-8 col-sm-12" src="/assets/img/biomorph/copper1.jpg" alt="Copper Preview Image"></a>
+        <div class="col section-content perspective ">
+            <h2 id="content" class="font1 font-change txt-color"><a href="deeptime.php">Deeptime</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <a href="deeptime.php" class="field-group-link project-link d-block p-visible font1 font-change">
+                        <div class="project-img bg">
+                            <img class="img-fluid" src="/assets/img/biomorph/deeptime1.jpg" alt="Deeptime Preview Image">
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
-        <div class="col-lg col-md-8 col-sm-12 mb-5">
-            <h2 class="font1 font-change txt-color"><a href="deeptime.php">Deeptime</h2>
-            <div id="content" class="col-lg col-md-8 col-sm-12">
-                <a href="deeptime.php"><img id="content" class="col-lg col-md-8 col-sm-12" src="/assets/img/biomorph/deeptime1.jpg" alt="Deeptime Preview Image"></a>
+        <div class="col section-content perspective ">
+            <h2 id="content" class="font1 font-change txt-color"><a href="hyperobjects.php">Hyperobjects</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <a href="hyperobjects.php" class="field-group-link project-link d-block p-visible font1 font-change">
+                        <div class="project-img bg">
+                            <img class="img-fluid" src="/assets/img/biomorph/hyperobjects1.jpg" alt="Hyperobjects Preview Image">
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
-        <div class="col-lg col-md-8 col-sm-12 mb-5">
-            <h2 class="font1 font-change txt-color"><a href="hyperobjects.php">Hyperobjects</h2>
-            <div id="content" class="col-lg col-md-8 col-sm-12">
-                <a href="hyperobjects.php"><img id="content" class="col-lg col-md-8 col-sm-12" src="/assets/img/biomorph/hyperobjects1.jpg" alt="Hyperobjects Preview Image"></a>
+        <div class="col section-content perspective ">
+            <h2 id="content" class="font1 font-change txt-color"><a href="contamination.php">Contamination</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <a href="contamination.php" class="field-group-link project-link d-block p-visible font1 font-change">
+                        <div class="project-img bg">
+                            <img class="img-fluid" src="/assets/img/biomorph/contamination1.jpg" alt="Contamination Preview Image">
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
 
-        <div class="col-lg col-md-8 col-sm-12 mb-5">
-            <h2 class="font1 font-change txt-color"><a href="contamination.php">Contamination</h2>
-            <div id="content" class="col-lg col-md-8 col-sm-12">
-                <a href="contamination.php"><img id="content" class="col-lg col-md-8 col-sm-12" src="/assets/img/biomorph/contamination1.jpg" alt="Contamination Preview Image"></a>
-            </div>
-        </div>
     </section>
 
     <?php include "footer.php"; ?>
