bb8f339fd8196b6665a0d1c04ece540ae1a586a4
tuckernickman <tucker.nickman@gmail.com>
2021-09-13 21:27:13 -0600

    linktree

---
+++ linktree.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Tucker Nickman - Portfolio Website 2021</title>
+    <?php include "header.php"; ?>
+</head>
+ 
+<body>
+
+    <?php include "menu.php"; ?>
+
+    <!--linktree-->
+
+    <!--ig--> <!--gh-->
+
+    <!--yt--> <!--sc-->
+
+    <!--tt--> <!--...-->
+
+    <!--os--> <!--f.a-->
+    
+    <!--fhd links--> <!--other-->
+
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
