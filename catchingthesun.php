f07179df592b1c7822b5698980181d7d96ab3172
tuckernickman <tucker.nickman@gmail.com>
2021-11-23 12:14:44 -0700

    general outline

---
+++ catchingthesun.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Biomorph</title>
+    
+    <?php include "extraheader.php"; ?>
+    <?php include "header.php"; ?>
+
+    <meta name="description" content="copper morph">
+</head>
+
+<body>
+
+    <?php include "menu.php"; ?>
+
+    <section>
+
+    <div class="col section-content perspective">
+
+        <div class="col section-content perspective">
+        <h2 id="content" class="txt-color">Copper</h2>
+            <div class="row gy-5">
+                <div id="content" class="col-sm">
+                    <div class="videoWrapper">
+                    <iframe id="content" class="col-sm html5-video-player" src="https://www.youtube.com/embed/CKNP2zSgQPo"w frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" preload="metadata" allowfullscreen></iframe>
+                    </div>
+                    <div class="project-year">
+                        <div class="blur" >
+		                    <h4 class="font1 font-change">
+                            <br><span>CONDUCTIVITY, SENDING PULSATING RIPPLES OF LIGHT THROUGH THE SCREEN.</span>
+                            <br><span>THE LANDSCAPE IN THE WAKE OF COLONIALISM.</span>
+                            <br><span>ITS BODY BEING PUSHED FURTHER INTO DEMOLITION.</span>
+                            <br><span>COLLECTING TONNES OF ROCK AND ORE, REFINING AND SEPARATING LEAD FROM COPPER.</span>
+                            <br><span>HEAVY METALS TUCKED AWAY DEEP IN THE EARTH, RESURFACED.</span>
+                            <br><span>CHURNED, MOLTEN LIQUID BECOMES EITHER COPPER OR THE TOXIC SLUDGE.</span>
+                            <br><span>RIBBONS OF ITS THREADS TANGLED IN E-WASTE,</span>
+                            <br><span>MERGING WITH THE AFTERMATH OF EXTRACTIVISM.</span>                      
+                            </h4>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+        
+        <div class="col section-content perspective">
+        <h2 id="content" class="txt-color">Artifacts of Forceful Occurrences</h2>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/artifacts/Ebene_9.png" alt="artifact 9">
+                    </div>
+                </a>
+            </div>
+        </div>
+    
+
+
+    </div>
+    </section>
+
+    <?php include "footer.php"; ?>
+
+</body>
