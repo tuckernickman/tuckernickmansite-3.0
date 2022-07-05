b6a4e3c3f300d5631e269996296da0c3c2e54af4
tuckernickman <tucker.nickman@gmail.com>
2022-07-05 13:02:56 -0600

    adjusted sizing

--- aboutme.php
+++ aboutme.php
@@ -1,11 +1,11 @@
 <section>
     <div class="container-fluid col" id="aboutContent">
-        <div id="textParagraph" class="col-sm">
+        <div id="textParagraph" class="col-md-8 col-sm-8 col-xs-8">
             <p class="txt-color"> Tucker Nickman is an artist, designer and aspiring web developer from Salt Lake City, with ten years of dance and creative experience.
              After earning their BFA of Stage Dance in 2018, in Berlin, Germany, they left to continue pursuing other creative outlets in Amsterdam and Utah.
               They are currently based here and working on various ongoing projects.</p>
         </div>
-        <div id="textParagraph" class="col-12">
+<!--         <div id="textParagraph" class="col-12">
             <button class="btn" onclick="myFunction()" id="demo" class="txt-color">click here</button>
-        </div>
+        </div> -->
 </section>
