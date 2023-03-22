b0556c2b98bfda656e7bdce85e7452ae0a02d05e
nektarck <64714910+nektarck@users.noreply.github.com>
2023-03-22 01:47:15 +0100

    adjusted column sizing

--- aboutme.php
+++ aboutme.php
@@ -1,6 +1,6 @@
 <section>
     <div class="container-fluid col" id="aboutContent">
-        <div id="textParagraph" class="col-md-8 col-sm-8 col-xs-8">
+        <div id="textParagraph" class="col-lg-12 col-md-12 col-sm-12 col-xs-8">
             <p class="txt-color"> Tucker Nickman is an artist, designer and aspiring web developer from Salt Lake City, with ten years of dance and creative experience.
              After earning their BFA of Stage Dance in 2018, in Berlin, Germany, they left to continue pursuing other creative outlets in Amsterdam and Utah.
               They are currently based here and working on various ongoing projects.</p>
