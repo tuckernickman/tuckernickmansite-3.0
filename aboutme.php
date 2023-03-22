fc4311fb226c4aa4cb6db1ff73ad31d44dae98e3
nektarck <64714910+nektarck@users.noreply.github.com>
2023-03-22 01:48:14 +0100

    edited font color

--- aboutme.php
+++ aboutme.php
@@ -1,7 +1,7 @@
 <section>
     <div class="container-fluid col" id="aboutContent">
         <div id="textParagraph" class="col-lg-12 col-md-12 col-sm-12 col-xs-8">
-            <p class="txt-color"> Tucker Nickman is an artist, designer and aspiring web developer from Salt Lake City, with ten years of dance and creative experience.
+            <p class="font1 font-change text-color"> Tucker Nickman is an artist, designer and aspiring web developer from Salt Lake City, with ten years of dance and creative experience.
              After earning their BFA of Stage Dance in 2018, in Berlin, Germany, they left to continue pursuing other creative outlets in Amsterdam and Utah.
               They are currently based here and working on various ongoing projects.</p>
         </div>
