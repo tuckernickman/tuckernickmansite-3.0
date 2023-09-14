791c816803c7f7be7fd16f417c3b50e043610f02
nektarck <64714910+nektarck@users.noreply.github.com>
2023-09-14 11:21:01 +0200

    img Grids for 2022/2023 to be updated with content

---
+++ imgGrid22.php
@@ -0,0 +1,103 @@
+<section>
+    
+    <div class="col section-content perspective">
+        <h2 id="content" class="txt-color">2019</h2>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/Wuste.jpg" alt="desert 1">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/kraschhh_2.jpg" alt="bones 1">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/IMG_3817edit.jpg" alt="2917 edit">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/IMG_3556.jpg" alt="IMG 3556">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/desert003.jpg" alt="desert 003">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/desert001.jpg" alt="desert 001">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/bones00072.jpg" alt="bones 2">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/1.1.7.jpg" alt="1.1.7">
+                    </div>
+                </a>
+            </div>
+            <div id="content" class="col">
+                <a href="" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="assets/img/2019/1.1.5.jpg" alt="1.1.5">
+                    </div>
+                </a>
+            </div>
+    </div>
+</section>
+
+<!-- 
+$images19 = [
+        "assets/img/2019/Wuste.jpg",
+        "assets/img/2019/lookout.jpg",
+        "assets/img/2019/kraschhh_2.jpg",
+        "assets/img/2019/IMG_3817edit.jpg",
+        "assets/img/2019/IMG_3632.jpg",
+        "assets/img/2019/IMG_3556.jpg",
+        "assets/img/2019/IMG_3556.jpg",
+        "assets/img/2019/desert003.jpg",
+        "assets/img/2019/ganstill.jpg",
+        "assets/img/2019/desert003.jpg",
+        "assets/img/2019/desert001.jpg",
+        "assets/img/2019/bones00072.jpg",
+        "assets/img/2019/1.1.7.jpg",
+        "assets/img/2019/1.1.5.jpg"
+    ];
+-->
+
+    <!--    
+            <div id="content" class="col-sm">
+                <a href="/2021.php" class="field-group-link project-link d-block p-visible">
+                    <div class="project-img bg">
+                        <img class="img-fluid" src="" alt="">
+                    </div>
+                    <div class="project-year">
+                        <small>
+
+                        </small>
+                    </div>
+                </a>
+            </div> 
+    -->
