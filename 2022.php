1684d900524a687565adf63ff849ee57060dc99f
nektarck <64714910+nektarck@users.noreply.github.com>
2023-09-14 11:20:38 +0200

    new pages for 2022/23 to be updated with content

---
+++ 2022.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>2022</title>
+    <?php include "header.php"; ?>
+</head>
+
+<body>
+    <?php include "menu.php"; ?>
+
+    <!--home page-->
+
+    <?php //include "section1.php"; ?>
+
+    <?php include "imgGrid21.php"?>
+
+    <?php //include "content1.php"; ?> 
+
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
---
+++ 2023.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>2023</title>
+    <?php include "header.php"; ?>
+</head>
+
+<body>
+    <?php include "menu.php"; ?>
+
+    <!--home page-->
+
+    <?php //include "section1.php"; ?>
+
+    <?php include "imgGrid21.php"?>
+
+    <?php //include "content1.php"; ?> 
+
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
